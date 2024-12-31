ce5e4d43f3cebf179fec23b01ee3b5a1f95649ab
hanna <hanharm@tamu.edu>
2024-12-31 15:25:34 +0800

    Add: movies table sorting

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+  def sortable_link(column, title)
+    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    icon = column == params[:sort] ? (params[:direction] == "asc" ? "▲" : "▼") : ""
+    link_to "#{title} #{icon}".html_safe, { sort: column, direction: direction }, class: "sortable-link"
+  end
 end
