4544b84f6480900bcf71e02c6f45e4a2c89658b8
hanna <hanharm@tamu.edu>
2024-12-31 14:44:27 +0800

    Add: movies to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,18 @@ more_movies = [
   { title: 'Nomadland', rating: 'R',
     release_date: '19-Feb-2021' },
   { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' }
+    release_date: '13-Aug-2021' },
+  { title: 'Gladiator', rating: 'R',
+    release_date: '1-May-2000' },
+  { title: 'Gladiator II', rating: 'R',
+    release_date: '14-Nov-2024' },
+  { title: 'Gladiator III', rating: 'R',
+    release_date: '20-Apr-2025' }
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by(title: movie[:title]) do |m|
+    m.rating = movie[:rating]
+    m.release_date = movie[:release_date]
+  end
 end
